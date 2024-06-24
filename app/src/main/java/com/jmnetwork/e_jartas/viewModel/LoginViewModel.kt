05e8348063205dc3ed1a14cec574c55320936de2
hnfnfl <kakaknaufal@gmail.com>
2024-06-24 17:12:36 +0700

    refactor: remove unused commented code

--- app/src/main/java/com/jmnetwork/e_jartas/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/jmnetwork/e_jartas/viewModel/LoginViewModel.kt
@@ -35,7 +35,6 @@ class LoginViewModel(application: Application) : ViewModel() {
 
     @SuppressLint("HardwareIds")
 
-
     fun login() {
         myPreferences = MySharedPreferences(appContext)
         val tokenAuth = BuildConfig.API_KEY
@@ -66,57 +65,4 @@ class LoginViewModel(application: Application) : ViewModel() {
             }
         }
     }
-
-//    fun login() {
-//        myPreferences = MySharedPreferences(appContext)
-//        val apiKey = BuildConfig.API_KEY
-//        myPreferences.setValue(Constants.TOKEN_AUTH, apiKey)
-//        val deviceID = Settings.Secure.getString(appContext.contentResolver, Settings.Secure.ANDROID_ID)
-//
-//        RetrofitClient.apiService.login(
-//            loginRequest.email,
-//            loginRequest.password,
-//            deviceID,
-//            apiKey
-//        ).enqueue(object : retrofit2.Callback<LoginResponse> {
-//            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-//                when (response.code()) {
-//                    200 -> {
-//                        val data = response.body()!!.data
-//                        myPreferences.setValue(Constants.USER, Constants.LOGIN)
-//                        myPreferences.setValueInteger(Constants.USER_IDADMIN, data.idadmin)
-//                        myPreferences.setValue(Constants.USER_EMAIL, data.email)
-//                        myPreferences.setValue(Constants.USER_NAMA, data.nama)
-//                        myPreferences.setValue(Constants.USER_ALAMAT, data.alamat)
-//                        myPreferences.setValue(Constants.USER_TELP, data.telp)
-//                        myPreferences.setValue(Constants.USER_FOTO, data.img)
-//                        myPreferences.setValue(Constants.USER_LEVEL, data.level)
-//                        myPreferences.setValueInteger(Constants.USER_IDLEVEL, data.idlevel)
-//                        _startActivityEvent.value = Constants.LOGIN
-//                    }
-//
-//                    401 -> {
-//                        _startActivityEvent.value = "Email atau kata sandi salah"
-//                        CustomHandler().responseHandler(appContext, "Login|onResponse", response.message())
-//                    }
-//
-//                    500 -> {
-//                        _startActivityEvent.value = "Internal server error"
-//                        CustomHandler().responseHandler(appContext, "Login|onResponse", response.message(), 500)
-//                    }
-//
-//                    else -> {
-//                        _startActivityEvent.value = response.errorBody().toString()
-//                        val errMsg = CustomHandler().parseError(response.errorBody()!!.string())
-//                        CustomHandler().responseHandler(appContext, "Login|onResponse", errMsg, response.code())
-//                    }
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-//                CustomHandler().responseHandler(appContext, "Login|onFailure", t.message.toString())
-//                _startActivityEvent.value = "failure"
-//            }
-//        })
-//    }
 }
